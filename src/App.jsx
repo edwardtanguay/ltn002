8e1a04d855c0ce0a3ff908d5b324a63200c3e655
Edward Tanguay on dell001 <edward@tanguay.info>
2023-02-01 01:28:50 +0100

    basic template

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-      
+        <h2>Skills to Learn</h2>
+        <ul>
+          <li>nnn</li>
+        </ul>
       </div>
     </div>
   )
